b78afa3863c90542885d63ddf60b2ce0d3adb9af
gurujaya <86539998+gurujaya@users.noreply.github.com>
2023-12-06 18:53:17 -0500

    animated text, links under the hero text

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@
 import Image from 'next/image'
 import logo from '../public/Rooted.png'
 import { motion } from 'framer-motion'
-import AnimatedTitle from './components/AnimatedTitle'
+import AnimatedText from './components/AnimatedText'
+import Link from 'next/Link'
 
 
 export default function Home() {
@@ -14,10 +15,12 @@ export default function Home() {
             <Image src={logo} alt="Rooted" className='w-full h-auto' />
           </div>
           <div className='w-1/2 flex flex-col items-center self-center'>
-            <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'>
-              <AnimatedTitle className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden"/>
+            <AnimatedText text="Rooted Together" className=' !text-left'/>
+            <p className='text-primary my-4 text-base font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <div className='flex items-center self-start mt-2'>
+              <Link href="https://hcb.hackclub.com/donations/start/rootedtogether" className="flex items-center bg-primary text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-primary border-2 border-solid border-transparent hover:border-primary" target={"_blank"}>Support us</Link>
+              <Link href="mailto:contact.rootedtogether@gmail.com" className='ml-4 text-lg font-medium capitalize text-primary underline' >Contact</Link>
             </div>
-            <p>At Rooted Together, we are passionate about urban agriculture and its potential to transform communities. Join us in our mission to create sustainable, thriving cities through the power of urban farming.</p>
           </div>
         </div>
       </div>
