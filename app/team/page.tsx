bea705842669f70af0074c48ff10fdaf68b33fd2
Abhay Gowda <131724712+AbhGow07@users.noreply.github.com>
2024-03-06 21:12:27 -0800

    Add files via upload

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -8,8 +8,107 @@ const page = () => {
         <div>
         <AnimatedText text='Our Team' className='mb-16'></AnimatedText>
         </div>
+
         <div>
-            
+          <div class="container my-24 mx-auto md:px-6">
+            <section class="mb-32 text-center">
+
+              <div class="lg:gap-xl-12 grid gap-x-6 md:grid-cols-3 xl:grid-cols-4">
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+
+                  <p class="mb-2 font-bold">Gurujaya</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">Co-founder</p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">[POSITION]</p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">
+                  [POSITION]
+                  </p>
+                </div>
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">[POSITION]</p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">
+                  [POSITION]
+                  </p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">[POSITION]</p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">[POSITION]</p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">[POSITION]</p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">[POSITION]</p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">
+                  [POSITION]
+                  </p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">
+                  [POSITION]
+                  </p>
+                </div>
+
+                <div class="mb-12">
+                  <img src=""
+                    class="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" style="max-width: 100px" />
+                  <p class="mb-2 font-bold">[ENTER NAME]</p>
+                  <p class="text-neutral-500 dark:text-neutral-300">[POSITION]</p>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
     </div>
   )
